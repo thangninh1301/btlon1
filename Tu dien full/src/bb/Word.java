8b43582c1f05689f95d17523fdd6895fcf06c49d
tdm0908 <hollycrapgotohell98@gmail.com>
2018-10-21 22:06:45 +0700

    ok

---
+++ Tu dien full/src/bb/Word.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bb;
+
+/**
+ *
+ * @author thang
+ */
+public class Word {
+    private String word_target;
+    private String word_explain;
+    
+}
